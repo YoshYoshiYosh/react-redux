aff78672c39665c30ac241d0edffcbe06355e874
Yoshiki Kato <y-katou@goem.co.jp>
2020-07-04 16:11:25 +0900

    counter app

--- src/App.js
+++ src/App.js
@@ -1,28 +1,36 @@
-import React from 'react';
-import PropTypes from 'prop-types';
+import React, { Component } from 'react';
 
-const App = () =>  {
-  const profiles = [
-    { name: "Taro",   age: 10 },
-    { name: "Hanako", age: 20 },
-    { name: "noname"}
-  ]
-  return (
-    <div>
-      {
-        profiles.map(({name, age}, index) => <User name={name} age={age} key={index} />)
-      }
-    </div>
-  )
-}
+const App = () => <Counter />
 
-const User = (props) => {
-  return <div>Hi, I'm {props.name}! I'm {props.age} years old.</div>
-}
+class Counter extends Component {
+  constructor(props) {
+    super(props)
+    this.state = { count: 0 }
+  }
 
-User.propTypes = {
-  name: PropTypes.string.isRequired,
-  age: PropTypes.number.isRequired
+  changeCount(plusOrMinus, changeValue) {
+    const changeNum = (plusOrMinus === 'plus') ? changeValue : -changeValue
+    this.setState({count: this.state.count + changeNum})
+  }
+
+  render() {
+    console.log("rendered.")
+    const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+    return (
+      <React.Fragment>
+        <div>count: {this.state.count}</div>
+        
+        {numbers.map((num, index) => {
+          return (
+            <React.Fragment key={index}>
+              <button onClick={() => this.changeCount('plus', num)}>+{num}</button>
+              <button onClick={() => this.changeCount('minus',num)}>-{num}</button>
+            </React.Fragment>
+          )
+        })}
+      </React.Fragment>
+    )
+  }
 }
 
 export default App;
